67168b11f9038cacf6710c11e1bbadb1a3931e45
Vipin Chaudhary <vipinchaudhary31122002@gmail.com>
2022-09-25 01:16:47 +0530

    fetched data from the database and have shown on the products list and delete the previous list of products of images. video covered upto 1:50

--- GadgetShop/app/views.py
+++ GadgetShop/app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Customer, Product, Cart, OrderPlaced
 
-def home(request):
- return render(request, 'app/home.html')
+# def home(request):
+#  return render(request, 'app/home.html')
+
+class ProductView(View):
+    def get(self, request):
+        laptops = Product.objects.filter(category='L')
+        mobiles = Product.objects.filter(category='M')
+        return render(request, 'app/home.html', {'laptops':laptops, 'mobiles':mobiles})
+
+
 
 def product_detail(request):
  return render(request, 'app/productdetail.html')
